aa6eb5fb548ff16cc3c61ccecae6df475b062e6e
Kislitsina-Maria <ma.kis@mail.ru>
2022-07-07 11:15:11 +0300

    Added new test and goals in chek report

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,11 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StatisticsServiceTest {
     @Test
-    void findMax() {
+    void findMaxIsFirst() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxIsSecond() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 15, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 15;
 
         long actual = service.findMax(incomesInBillions);
 
